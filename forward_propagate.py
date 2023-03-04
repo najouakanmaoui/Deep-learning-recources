5958f72f1a9e30626664bf0c29756ab153a77871
= <=>
2023-03-04 21:56:44 +0100

    forward_propagate_function

---
+++ forward_propagate.py
@@ -0,0 +1,24 @@
+def forward_propagate(network, inputs):
+    
+    layer_inputs = list(inputs) # start with the input layer as the input to the first hidden layer
+    
+    for layer in network:
+        
+        layer_data = network[layer]
+        
+        layer_outputs = [] 
+        for layer_node in layer_data:
+        
+            node_data = layer_data[layer_node]
+        
+            # compute the weighted sum and the output of each node at the same time 
+            node_output = node_activation(compute_weighted_sum(layer_inputs, node_data['weights'], node_data['bias']))
+            layer_outputs.append(np.around(node_output[0], decimals=4))
+            
+        if layer != 'output':
+            print('The outputs of the nodes in hidden layer number {} is {}'.format(layer.split('_')[1], layer_outputs))
+    
+        layer_inputs = layer_outputs # set the output of this layer to be the input to next layer
+
+    network_predictions = layer_outputs
+    return network_predictions
